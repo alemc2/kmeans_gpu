0f5ca9828f2cfe803b791e08015d6d8ba27db3c5
alemc2 <alemc2@gmail.com>
2016-12-06 00:34:25 -0500

    adding kernel for calculating centroids given membership. Again only checked for syntax, functionality needs to be tested

--- kmeans_lloyd.cu
+++ kmeans_lloyd.cu
@@ -16,13 +16,6 @@ __device__ float distance( float *sample1, float * sample2, uint32_t
         incr1, uint32_t incr2);
 
 
-cudaError_t kmeans_cuda( InitMethod init, float tolerance, uint32_t num_samples,
-        uint32_t num_features, uint32_t num_clusters_size, uint32_t seed, const
-        float *samples, float *centroids, uint32_t *memberships)
-{
-    return cudaSuccess;
-}
-
 // Currently handled as an array, contingent upon caller to coalesce access.
 // sample1 - memory location from which to read the data for point 1. Typically
 // in our case going to be the dataset we are looking at.
@@ -45,7 +38,7 @@ __device__ float distance( float *sample1, float *sample2, uint32_t incr1,
 }
 
 __global__ void nearest_cluster_assign( float *samples, float *centroids,
-        uint32_t *membership)
+        uint32_t *membership, uint32_t *membership_old)
 {
     uint32_t sample_idx = blockIdx.x * blockDim.x + threadIdx.x;
     float min_dist = FLT_MAX;
@@ -100,5 +93,85 @@ __global__ void nearest_cluster_assign( float *samples, float *centroids,
             }
         }
     }
+    membership_old[sample_idx] = membership[sample_idx];
     membership[sample_idx] = nearest_cluster;
 }
+
+__global__ void adjust_centroids( float *samples, float *centroids, uint32_t
+        *membership, uint32_t *membership_old, uint32_t *cluster_counts)
+{
+    uint32_t centroid_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(centroid_idx >= num_clusters)
+        return;
+    centroids += centroid_idx;
+    uint32_t cluster_count = cluster_counts[centroid_idx];
+    //multiply each centroid by it's count to make it ready for adjustments -
+    //neccessary evil of globalmemory writes
+    for(uint32_t i = 0; i < num_features_dim; i++)
+    {
+        centroids[i*num_clusters] *= cluster_count;
+    }
+    extern __shared__ uint32_t shared_memberships[];
+    //Since each membership is of type uint32_t and we have old and new
+    //memberships, we can load the memberships to fill half the shared memory
+    uint32_t sample_step = shmem_size/(2*sizeof(uint32_t));
+    uint32_t samples_per_thread = ceilf(sample_step/min(blockDim.x, num_clusters
+                - blockDim.x * blockDim.x));
+    for(uint32_t sample_start = 0; sample_start < num_samples; sample_start +=
+            sample_step)
+    {
+        for(uint32_t i=0; i<samples_per_thread; i++)
+        {
+            uint32_t local_offset = i * samples_per_thread + threadIdx.x;
+            uint32_t global_offset = local_offset + sample_start;
+            if(global_offset < num_samples && local_offset < sample_step)
+            {
+                shared_memberships[2*local_offset] = membership[global_offset];
+                shared_memberships[2*local_offset+1] = membership_old[global_offset];
+            }
+        }
+        __syncthreads();
+        //Now each thread is going to scan all the shared samples
+        for(uint32_t i=0; i < sample_step && sample_start + i < num_samples;
+                i++)
+        {
+            uint32_t local_membership = shared_memberships[2*i];
+            uint32_t local_membership_prev = shared_memberships[2*i+1];
+            int sign = 0;
+            if(local_membership_prev == centroid_idx && local_membership !=
+                    centroid_idx)
+            {
+                sign = -1;
+                cluster_count--;
+            }
+            else if(local_membership_prev != centroid_idx && local_membership ==
+                    centroid_idx)
+            {
+                sign = 1;
+                cluster_count++;
+            }
+            if(sign)
+            {
+                uint32_t sample_offset = sample_start + i;
+                for(uint32_t feature = 0; feature < num_features_dim; feature++)
+                {
+                    centroids[feature * num_clusters] += sign *
+                        samples[sample_offset + feature * num_samples];
+                }
+            }
+        }
+    }
+    // Average the centroid
+    for(uint32_t i = 0; i < num_features_dim; i++)
+        centroids[i*num_clusters] /= cluster_count;
+    //Write back local count to memory
+    cluster_counts[centroid_idx] = cluster_count;
+}
+
+cudaError_t kmeans_cuda( InitMethod init, float tolerance, uint32_t num_samples,
+        uint32_t num_features, uint32_t num_clusters_size, uint32_t seed, const
+        float *samples, float *centroids, uint32_t *memberships)
+{
+    return cudaSuccess;
+}
+
